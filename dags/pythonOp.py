a88654cc8c4368c9b6cef37052298a6ca8387050
kyeongseooh <83737055+kyeongseooh@users.noreply.github.com>
2023-09-22 05:36:39 +0900

    Update pythonOp.py

--- dags/pythonOp.py
+++ dags/pythonOp.py
@@ -3,13 +3,38 @@ from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.utils.dates import days_ago
 import pendulum
+from kubernetes.client import models as k8s
 
+volume_mount = k8s.V1VolumeMount(name='airflow-shared-pv',
+       mount_path="/airflow/shared",
+       sub_path=None,
+       read_only=False)
+
+pvc = k8s.V1PersistentVolumeClaimVolumeSource(claim_name="osm-config-pv-claim")
+
+volume = k8s.V1Volume(name="airflow-shared-pv", persistent_volume_claim=pvc)
+
+
+executor_config={
+              "pod_override": k8s.V1Pod(
+                  spec=k8s.V1PodSpec(
+                      containers=[
+                          k8s.V1Container(
+                              name="base",
+                              volume_mounts=[volume_mount]
+                          )
+                      ],
+                      volumes=[volume],
+                  )
+              )
+          }
 
 dag = DAG(
     dag_id='pythonOp',
     # test용으로 한번만 돌릴 때 @once를 사용
     schedule_interval='@once',
-    start_date=datetime(2023,8,21,10,17)
+    start_date=datetime(2023,8,21,10,17),
+    executor_config=executor_config
     # schedule=timedelta(minutes=1)
     )
 
@@ -20,10 +45,10 @@ def createFile(location, filename):
         f.write('test')
 
 # op_kwargs는 dict, op_args는 list
-a = PythonOperator(task_id="t1", python_callable=createFile, op_kwargs={'location': '/mnt/shared','filename': 'touch.txt'}, dag=dag)
+a = PythonOperator(task_id="t1", python_callable=createFile, op_kwargs={'location': '/airflow/shared','filename': 'touch.txt'}, dag=dag)
 
 def catFile():
-    with open(f"/mnt/shared/touch.txt", 'r') as f:
+    with open(f"/airflow/shared/touch.txt", 'r') as f:
         text = f.read()
     print(text)
 b = PythonOperator(task_id="t2", python_callable=catFile,  dag=dag)
