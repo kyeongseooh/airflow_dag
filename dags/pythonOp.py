a168fe0b06b0c81d117439d8c683e9692925960d
kyeongseooh <83737055+kyeongseooh@users.noreply.github.com>
2023-09-22 05:45:34 +0900

    Update pythonOp.py

--- dags/pythonOp.py
+++ dags/pythonOp.py
@@ -4,30 +4,33 @@ from datetime import datetime, timedelta
 from airflow.utils.dates import days_ago
 import pendulum
 from kubernetes.client import models as k8s
+K8S_VOLUMES=[
+    k8s.V1Volume(
+        name='airflow-shared-pv',
+        persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name='airflow-shared-pv'),
+    )
+]
 
-volume_mount = k8s.V1VolumeMount(name='airflow-shared-pv',
-       mount_path="/airflow/shared",
-       sub_path=None,
-       read_only=False)
+K8S_VOLUME_MOUNTS=[
+    k8s.V1VolumeMount(name='airflow-shared-pv', mount_path='/shared/')
+]
 
-pvc = k8s.V1PersistentVolumeClaimVolumeSource(claim_name="osm-config-pv-claim")
-
-volume = k8s.V1Volume(name="airflow-shared-pv", persistent_volume_claim=pvc)
-
-
-executor_config={
-              "pod_override": k8s.V1Pod(
-                  spec=k8s.V1PodSpec(
-                      containers=[
-                          k8s.V1Container(
-                              name="base",
-                              volume_mounts=[volume_mount]
-                          )
-                      ],
-                      volumes=[volume],
-                  )
-              )
-          }
+executor_config = {
+    "pod_override": k8s.V1Pod(
+        metadata=k8s.V1ObjectMeta(
+            namespace='airflow'
+        ),
+        spec=k8s.V1PodSpec(
+            containers=[
+                k8s.V1Container(
+                    name='base',
+                    volume_mounts=K8S_VOLUME_MOUNTS,
+                )
+            ],
+            volumes=K8S_VOLUMES,
+        )
+    )
+}
 
 dag = DAG(
     dag_id='pythonOp',
@@ -44,10 +47,10 @@ def createFile(location, filename):
         f.write('test')
 
 # op_kwargs는 dict, op_args는 list
-a = PythonOperator(task_id="t1", python_callable=createFile, op_kwargs={'location': '/airflow/shared','filename': 'touch.txt'}, dag=dag, executor_config=executor_config)
+a = PythonOperator(task_id="t1", python_callable=createFile, op_kwargs={'location': '/shared','filename': 'touch.txt'}, dag=dag, executor_config=executor_config)
 
 def catFile():
-    with open(f"/airflow/shared/touch.txt", 'r') as f:
+    with open(f"/shared/touch.txt", 'r') as f:
         text = f.read()
     print(text)
 b = PythonOperator(task_id="t2", python_callable=catFile,  dag=dag, executor_config=executor_config)
