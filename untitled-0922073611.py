154649498300ceb0818aff9ee4d7bd33138056bb
kyeongseooh <83737055+kyeongseooh@users.noreply.github.com>
2023-09-22 16:37:28 +0900

    Pushed DAG untitled-0922073611

---
+++ untitled-0922073611.py
@@ -0,0 +1,89 @@
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.contrib.kubernetes.volume_mount import VolumeMount
+from airflow.contrib.kubernetes.volume import Volume
+from airflow.kubernetes.secret import Secret
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+args = {
+    "project_id": "untitled-0922073611",
+}
+
+dag = DAG(
+    "untitled-0922073611",
+    default_args=args,
+    schedule_interval="@once",
+    start_date=days_ago(1),
+    description="""
+Created with Elyra 3.15.0 pipeline editor using `untitled.pipeline`.
+    """,
+    is_paused_upon_creation=False,
+)
+
+
+# Operator source: Untitled.ipynb
+
+op_f48c0d40_687e_422c_9c6a_341e48f2dae0 = KubernetesPodOperator(
+    name="Untitled",
+    namespace="airflow",
+    image="continuumio/anaconda3@sha256:a2816acd3acda208d92e0bf6c11eb41fda9009ea20f24e123dbf84bb4bd4c4b8",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/elyra/airflow/bootstrapper.py' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/elyra/airflow/bootstrapper.py --output bootstrapper.py && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/etc/generic/requirements-elyra.txt' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/etc/generic/requirements-elyra.txt --output requirements-elyra.txt && python3 -m pip install packaging && python3 -m pip freeze > requirements-current.txt && python3 bootstrapper.py --pipeline-name 'untitled' --cos-endpoint http://minio-service.kubeflow.svc.cluster.local:9000 --cos-bucket kubeflow-user-example-com --cos-directory 'untitled-0922073611' --cos-dependencies-archive 'Untitled-f48c0d40-687e-422c-9c6a-341e48f2dae0.tar.gz' --file 'Untitled.ipynb' "
+    ],
+    task_id="Untitled",
+    env_vars={
+        "ELYRA_RUNTIME_ENV": "airflow",
+        "AWS_ACCESS_KEY_ID": "minio",
+        "AWS_SECRET_ACCESS_KEY": "minio123",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+        "ELYRA_RUN_NAME": "untitled-{{ ts_nodash }}",
+    },
+    resources={
+        "request_cpu": "1",
+        "request_memory": "1G",
+    },
+    volumes=[],
+    volume_mounts=[],
+    secrets=[],
+    annotations={},
+    labels={},
+    tolerations=[],
+    in_cluster=True,
+    config_file="None",
+    dag=dag,
+)
+
+
+# Operator source: Untitled.ipynb
+
+op_5aa3f2e1_1896_4dd4_bac5_aba6c7d2a46f = KubernetesPodOperator(
+    name="Untitled_1",
+    namespace="airflow",
+    image="apache/airflow:latest",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/elyra/airflow/bootstrapper.py' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/elyra/airflow/bootstrapper.py --output bootstrapper.py && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/etc/generic/requirements-elyra.txt' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/etc/generic/requirements-elyra.txt --output requirements-elyra.txt && python3 -m pip install packaging && python3 -m pip freeze > requirements-current.txt && python3 bootstrapper.py --pipeline-name 'untitled' --cos-endpoint http://minio-service.kubeflow.svc.cluster.local:9000 --cos-bucket kubeflow-user-example-com --cos-directory 'untitled-0922073611' --cos-dependencies-archive 'Untitled-5aa3f2e1-1896-4dd4-bac5-aba6c7d2a46f.tar.gz' --file 'Untitled.ipynb' "
+    ],
+    task_id="Untitled_1",
+    env_vars={
+        "ELYRA_RUNTIME_ENV": "airflow",
+        "AWS_ACCESS_KEY_ID": "minio",
+        "AWS_SECRET_ACCESS_KEY": "minio123",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+        "ELYRA_RUN_NAME": "untitled-{{ ts_nodash }}",
+    },
+    resources={
+        "request_cpu": "1",
+        "request_memory": "1G",
+    },
+    volumes=[],
+    volume_mounts=[],
+    secrets=[],
+    annotations={},
+    labels={},
+    tolerations=[],
+    in_cluster=True,
+    config_file="None",
+    dag=dag,
+)
